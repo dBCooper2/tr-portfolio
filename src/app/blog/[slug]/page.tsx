9c3837280f67faa696dc1852e24746874709b6df
dBCooper2 <t.rowland711@gmail.com>
2024-04-19 12:01:26 -0500

    articles should render now

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,9 +1,7 @@
 import { fetchMarkdownContent } from '@/utils/markdown';
-import { generatePathname } from '@/utils/pathnameUtils';
 import MarkdownContent from '@/components/MarkdownContent';
 import articles from '@/content/articles.json';
 import Foot from '@/app/foot';
-
 
 export default async function ArticlePage({ params }: { params: { slug: string } }) {
   const { slug } = params;
@@ -12,10 +10,16 @@ export default async function ArticlePage({ params }: { params: { slug: string }
   return (
     <div className="container mx-auto">
       <div className="p-10 lg:p-24">
-        <MarkdownContent content={content}/>
+        <MarkdownContent content={content} />
       </div>
     </div>
   );
+}
+
+export async function generateStaticParams() {
+  return articles.map((article) => ({
+    slug: article.slug,
+  }));
 }
 
 export async function generateMetadata({ params }: { params: { slug: string } }) {
