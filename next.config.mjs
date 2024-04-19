8ee81d07161db12403875e607fdb89a3e4978fa5
dBCooper2 <t.rowland711@gmail.com>
2024-04-19 12:17:48 -0500

    should be done for now...

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    compress: true, // Enable compression by default
+    images: {
+        formats: ['image/webp', 'image/avif'],
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'example.com',
+            port: '',
+            pathname: '/images/**',
+          },
+        ],
+      },
+  };
+  
+  export default nextConfig;
