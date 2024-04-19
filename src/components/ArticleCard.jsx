7482546a17bff3788761c1f8e054919a344516ce
dBCooper2 <t.rowland711@gmail.com>
2024-04-18 21:52:50 -0500

    changed link method

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -12,31 +12,32 @@ export default function ArticleCard({ article }) {
   };
 
   return (
-    <div
-      className="bg-cBlock p-2 rounded-lg shadow-md mb-4 cursor-pointer"
-      onClick={handleArticleClick}
-    >
-      <h3 className="markdown-h3">{article.title}</h3>
-      <p className="markdown-paragraph">{article.description}</p>
-      <div className="flex flex-wrap gap-2 mt-4">
-        {article.tags.map((tag, index) => {
-          const tagInfo = tagData.find((t) => t.text.toLowerCase() === tag.toLowerCase());
-          return (
-            <div
-              key={index}
-              style={{
-                backgroundColor: tagInfo ? tagInfo.color : '#2142ab',
-                color: 'white',
-                borderRadius: '9999px',
-                padding: '0.25rem 0.75rem',
-                fontWeight: 500,
-              }}
-            >
-              {tag}
-            </div>
-          );
-        })}
+    <div>
+      <a href={`/blog/${article.slug}`}>
+      <div className="bg-cBlock p-2 rounded-lg shadow-md mb-4 cursor-pointer">
+        <h3 className="markdown-h3">{article.title}</h3>
+        <p className="markdown-paragraph">{article.description}</p>
+        <div className="flex flex-wrap gap-2 mt-4">
+          {article.tags.map((tag, index) => {
+            const tagInfo = tagData.find((t) => t.text.toLowerCase() === tag.toLowerCase());
+            return (
+              <div
+                key={index}
+                style={{
+                  backgroundColor: tagInfo ? tagInfo.color : '#2142ab',
+                  color: 'white',
+                  borderRadius: '9999px',
+                  padding: '0.25rem 0.75rem',
+                  fontWeight: 500,
+                }}
+              >
+                {tag}
+              </div>
+            );
+          })}
+        </div>
       </div>
+      </a>
     </div>
   );
 }
